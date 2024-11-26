c9b3aecd1af14f39b2dd311f096a24dbea18d5aa
Dr.Lt.Data <dr.lt.data@gmail.com>
2024-11-26 18:33:27 +0900

    fix: install script exception handling for torchvision.download_url

--- install.py
+++ install.py
@@ -114,19 +114,22 @@ try:
         onnx_path = os.path.join(model_path, "onnx")
 
         if not os.path.exists(os.path.join(os.path.dirname(__file__), '..', 'skip_download_model')):
-            if not impact.config.get_config()['mmdet_skip']:
-                bbox_path = os.path.join(model_path, "mmdets", "bbox")
-                if not os.path.exists(bbox_path):
-                    os.makedirs(bbox_path)
+            try:
+                if not impact.config.get_config()['mmdet_skip']:
+                    bbox_path = os.path.join(model_path, "mmdets", "bbox")
+                    if not os.path.exists(bbox_path):
+                        os.makedirs(bbox_path)
 
-                if not os.path.exists(os.path.join(bbox_path, "mmdet_anime-face_yolov3.pth")):
-                    download_url("https://huggingface.co/dustysys/ddetailer/resolve/main/mmdet/bbox/mmdet_anime-face_yolov3.pth", bbox_path)
+                    if not os.path.exists(os.path.join(bbox_path, "mmdet_anime-face_yolov3.pth")):
+                        download_url("https://huggingface.co/dustysys/ddetailer/resolve/main/mmdet/bbox/mmdet_anime-face_yolov3.pth", bbox_path)
 
-                if not os.path.exists(os.path.join(bbox_path, "mmdet_anime-face_yolov3.py")):
-                    download_url("https://raw.githubusercontent.com/Bing-su/dddetailer/master/config/mmdet_anime-face_yolov3.py", bbox_path)
+                    if not os.path.exists(os.path.join(bbox_path, "mmdet_anime-face_yolov3.py")):
+                        download_url("https://raw.githubusercontent.com/Bing-su/dddetailer/master/config/mmdet_anime-face_yolov3.py", bbox_path)
 
-            if not os.path.exists(os.path.join(sam_path, "sam_vit_b_01ec64.pth")):
-                download_url("https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth", sam_path)
+                if not os.path.exists(os.path.join(sam_path, "sam_vit_b_01ec64.pth")):
+                    download_url("https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth", sam_path)
+            except:
+                print(f"[Impact Pack] Failed to auto-download model files. Please download them manually.")
 
         if not os.path.exists(onnx_path):
             print(f"### ComfyUI-Impact-Pack: onnx model directory created ({onnx_path})")
